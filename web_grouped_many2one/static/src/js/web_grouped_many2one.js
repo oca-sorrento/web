dada430dbbe4c9fb422893be6513c5bf47aacc00
tarteo <dennissluijk@outlook.com>
2016-04-28 17:04:33 +0200

    Initial widget

---
+++ web_grouped_many2one/static/src/js/web_grouped_many2one.js
@@ -0,0 +1,73 @@
+odoo.define('web_grouped_many2one.form_relational', function(require) { 
+"use strict";
+
+var form_relational = require('web.form_relational');
+var core = require('web.core');
+var data = require('web.data');
+
+var FieldMany2One = core.form_widget_registry.get('many2one');
+
+var FieldGroupedMany2One = FieldMany2One.extend({
+    render_editable: function() { 
+        var self = this;
+        this._super.apply(this, arguments);
+        
+        this.$input.autocomplete({
+            source: function(req, resp) {
+                self.get_search_result(req.term).done(function(result) {
+                    resp(result);
+                });
+            },
+        });
+    },
+    get_search_result: function() {
+        var self = this;
+        var group_by = 'country_id'; //for testing perposes
+        var result = this._super.apply(this, arguments);
+        return result.then(function(d) {
+            //Get group by field values
+            var dataset = new data.DataSet(self, self.field.relation, self.build_context());
+            //Remove the undefined ids (Search more buttons)
+            var dr = _.filter(d, function(r) {
+                return typeof r.id != 'undefined';
+            });
+            //Get list of ids
+            var ids = _.map(dr, function(r) {
+                return r.id;
+            });
+            var read = dataset.read_ids(ids, [group_by]);
+            
+            return read.then(function(records) {
+                //Get the groups
+                var groups = _.groupBy(records, function(r) { 
+                    return r[group_by][1];
+                });
+                var items = [];
+                //For each group add the group and the records of it
+                for (var group in groups) {
+                    items.push({
+                        label: '<b>' + group + '</b>',
+                        value: group,
+                        name: group,
+                        id: null
+                    });
+                    for (var i in groups[group]) {
+                        //Find the already fetched name of the record
+                        var get = _.filter(dr, function(d) { return d.id == groups[group][i].id; })[0];
+                        items.push({
+                            label: get['label'],
+                            value: get['value'],
+                            name: get['name'],
+                            id: get['id']
+                        });
+                    }
+                }
+                return items;
+            });
+            
+        });
+    }
+});
+
+core.form_widget_registry.add('grouped_many2one', FieldGroupedMany2One)
+});
